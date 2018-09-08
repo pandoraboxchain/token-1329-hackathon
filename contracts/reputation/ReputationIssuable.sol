65bf5610729ead28cfe3e43a8a45877261e14722
Andrey Sobol <sobol@pandora.foundation>
2018-09-08 16:27:53 +0200

    ReputationIssuable

---
+++ contracts/reputation/ReputationIssuable.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.24;
+
+import "./Reputation.sol";
+
+
+contract ReputationIssuable is Reputation {
+
+    function issueByAuth(address auth, uint256 value) public {
+        require(_owner_addresses[auth] != address(0));
+
+        address owner = _owner_addresses[auth];
+        uint256 duration = _authorized_duration[auth];
+        if (duration >= block.number) {
+            // TODO: Banned
+            _balances[owner] += value;
+        }
+    }
+
+}
