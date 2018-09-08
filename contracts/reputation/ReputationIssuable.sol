06b01284762a498466c76a5c7c45e6952e53c13f
Andrey Sobol <sobol@pandora.foundation>
2018-09-08 20:15:26 +0200

    banned in issueByAuth

--- contracts/reputation/ReputationIssuable.sol
+++ contracts/reputation/ReputationIssuable.sol
@@ -9,9 +9,11 @@ contract ReputationIssuable is Reputation {
         require(_owner_addresses[auth] != address(0));
 
         address owner = _owner_addresses[auth];
+
+        require(!_banneds[owner]);
+
         uint256 duration = _authorized_duration[auth];
         if (duration >= block.number) {
-            // TODO: Banned
             _balances[owner] += value;
             emit Issued(owner, value);
         } else {
