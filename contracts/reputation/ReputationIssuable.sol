1e0b5a575b2b5b22abf10317e26418a90b6da45d
Andrey Sobol <sobol@pandora.foundation>
2018-09-08 16:37:04 +0200

    emit Issued

--- contracts/reputation/ReputationIssuable.sol
+++ contracts/reputation/ReputationIssuable.sol
@@ -13,6 +13,7 @@ contract ReputationIssuable is Reputation {
         if (duration >= block.number) {
             // TODO: Banned
             _balances[owner] += value;
+            emit Issued(owner, value);
         }
     }
 
